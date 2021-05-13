632c68608fa1af5044543ba7f560be53ded94062
dvalley56 <isasunasra007@gmail.com>
2021-05-13 17:10:12 +0530

    FIXED APP MODULE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { LayoutComponent } from './layout/layout.component';
-import { LayoutSidemenuComponent } from './layout/layout-sidemenu/layout-sidemenu.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
+//Custom
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutToolbarComponent } from './layout/layout-toolbar/layout-toolbar.component';
+import { LayoutSidemenuComponent } from './layout/layout-sidemenu/layout-sidemenu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
+    LayoutToolbarComponent,
     LayoutSidemenuComponent
   ],
   imports: [
