b012cc307372aeaeb3d5e4f37dadd2bec90fad70
dvalley56 <isasunasra007@gmail.com>
2021-05-13 16:22:01 +0530

    ABOUT PAGE ADDED

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
   }
+  ngOnInit() {
+    
+  }
+ 
 
 }
